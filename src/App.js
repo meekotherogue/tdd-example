9c96f4a97f79504c0515b6074de6a2105d49d24c
Martina Nagy <martina@7shifts.com>
2020-02-23 16:29:18 -0600

    Import the form in App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import AlgorithmDisplayer from './AlgorithmDisplayer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <p className="App-intro">Hello World!</p>
+        <AlgorithmDisplayer />
       </div>
     );
   }
