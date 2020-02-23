728276b1147b347dad5008af6dc80c984f90c69f
Martina Nagy <martina@7shifts.com>
2020-02-23 14:59:46 -0600

    Removing things we don't need

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import App from './App';
 import { render } from '@testing-library/react';
 
-it('renders without crashing', () => {
-  render(<App />);
+it('renders without crashing and displays Hello World', () => {
+  const {getByText} = render(<App />);
+  getByText(/hello world/i);
 });
